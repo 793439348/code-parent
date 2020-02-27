a01cdaf3593abe6fa8a8d280a2de5df23e7c6857
木头人 <5423279+zt97@user.noreply.gitee.com>
2020-02-27 23:26:45 +0800

    Initial commit

---
+++ src/main/java/com/code/constant/Constant.java
@@ -0,0 +1,14 @@
+package com.code.constant;
+
+/**
+ * Created by zezhong.shang on 17-6-5.
+ */
+public class Constant {
+
+    public static String template_path = "D:\\workspace\\itripbackend\\code-parent\\src\\main\\resources\\itrip";
+
+    public static String service_save_path = "D://workspace//test";
+    public static String servie_impl_save_path = "D://workspace//test";
+    public static String mapper_save_path = "D://workspace//itrip//test";
+    public static String model_save_path = "D://workspace//itrip//test";
+}
